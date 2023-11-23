366b4d676d4fca062df868a23ac9072ecb039ee2
김원 <ejxldnjs13@gmail.com>
2023-11-23 15:31:50 +0900

    에셋 추가

---
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/AI/State/AttackState.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackState : AIState
+{
+    private float lastAttackTime;
+    private float attackCoolTime;
+    public AttackState(AIController aiController) : base(aiController)
+    {
+        attackCoolTime = aiController.attackInfos[(int)SkillTypes.Base].cooldown;
+    }
+
+    public override void Enter()
+    {
+        agent.speed = 0;
+        lastAttackTime -= attackCoolTime;
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void Update()
+    {
+        //Debug.Log("Attack State");
+        RotateToTarget();
+
+        var dirToTarget = aiController.target.position - aiTr.position;
+        dirToTarget.Normalize();
+
+        if (aiController.RaycastToTarget)
+        {
+            if (lastAttackTime + attackCoolTime < Time.time)
+            {
+                lastAttackTime = Time.time;
+                if (aiController.attackInfos[(int)SkillTypes.Base] != null)
+                    aiController.attackInfos[(int)SkillTypes.Base].ExecuteAttack(aiController.gameObject, aiController.target.gameObject);
+
+            }
+        }
+    }
+}
