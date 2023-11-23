f172c0d81533f5865b885c3bd7418e9dfba27223
김원 <ejxldnjs13@gmail.com>
2023-11-24 00:31:42 +0900

    1주차 빌드버전v1

--- Underground_Gamers/Assets/Lobby Scene Assets/Scripts/Universal/Player/PlayerLoadManager.cs
+++ Underground_Gamers/Assets/Lobby Scene Assets/Scripts/Universal/Player/PlayerLoadManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class PlayerLoadManager : MonoBehaviour
 {
