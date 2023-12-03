1e684c75bc103d74778974683db0c6985aea2761
오재영 <ostyoung@naver.com>
2023-12-04 00:13:51 +0900

    방향 수정

---
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/LookCameraByScale.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookCameraByScale : MonoBehaviour
+{
+    private RectTransform rect;
+    private Vector3 cameraRight;
+
+    private void Awake()
+    {
+        rect = transform.GetChild(1).GetComponent<RectTransform>();
+        cameraRight = Camera.main.transform.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dot = Vector3.Dot(transform.forward, cameraRight);
+
+        Vector3 newScale = rect.localScale;
+        if (dot > 0)
+        {           
+            newScale.x = -1;
+        }
+        else
+        {
+            newScale.x = 1;
+        }
+
+        rect.localScale = newScale;
+    }
+}
