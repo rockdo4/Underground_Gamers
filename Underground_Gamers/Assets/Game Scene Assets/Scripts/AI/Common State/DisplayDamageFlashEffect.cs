fc37c08143e7b53040b2246bd6b2b43bc0e1a46f
오재영 <ostyoung@naver.com>
2023-12-08 12:09:29 +0900

    피격 이펙트 구현 완료 / 데미지 텍스트, 점등효과

---
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/AI/Common State/DisplayDamageFlashEffect.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayDamageFlashEffect : MonoBehaviour, IAttackable
+{
+    private Color originColor = Color.white;
+    private Color flashColor = Color.red;
+    public float duration = 0.3f;
+    private float startTime;
+    private bool isHit;
+
+    private void Update()
+    {
+        if(isHit)
+        {
+            float t = (Time.time - startTime) / duration;
+            Color lerpColor = Color.Lerp(flashColor, originColor, t);
+            FlashRenderer(lerpColor);
+            if(t > 1)
+                isHit = false;
+        }
+    }
+    public void OnAttack(GameObject attacker, Attack attack)
+    {
+        isHit = true;
+        startTime = Time.time;
+        FlashRenderer(flashColor);
+    }
+
+    private void FlashRenderer(Color newColor)
+    {
+        var renderers = GetComponentsInChildren<Renderer>();
+        foreach(Renderer renderer in renderers)
+        {
+            renderer.material.color = newColor;
+        }
+    }
+}
