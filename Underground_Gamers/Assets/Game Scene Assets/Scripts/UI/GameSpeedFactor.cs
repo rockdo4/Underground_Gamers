78bde8d69585d9d391281a29fb09a567544af3c7
오재영 <ostyoung@naver.com>
2023-12-12 00:05:49 +0900

    게임 스피드 팩터 구현

---
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/GameSpeedFactor.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSpeedFactor : MonoBehaviour
+{
+    public GameObject factorX1;
+    public GameObject factorX2;
+    public GameObject factorX4;
+
+    public void OnSpeedFactorX1()
+    {
+        factorX4.SetActive(false);
+        factorX1.SetActive(true);
+        Time.timeScale = 1.0f;
+    }
+    public void OnSpeedFactorX2()
+    {
+        factorX1.SetActive(false);
+        factorX2.SetActive(true);
+        Time.timeScale = 2.0f;
+    }
+    public void OnSpeedFactorX4()
+    {
+        factorX2.SetActive(false);
+        factorX4.SetActive(true);
+        Time.timeScale = 4.0f;
+    }
+}
