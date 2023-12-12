5945bfc4fe27fbb5029e0b46efb6756b4ea63a2b
오재영 <ostyoung@naver.com>
2023-12-12 23:44:37 +0900

    라인 스위치 버그 수정 : 기존에 있던 라인에 한번 더 드랍할 시 에 대한 예외처리

--- Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/OnDrop.cs
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/OnDrop.cs
@@ -1,30 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class OnDrop : MonoBehaviour, IDropHandler
 {
     public CommandManager commandManager;
+
     void IDropHandler.OnDrop(PointerEventData eventData)
     {
+
         GameObject gameObject = DragAndDrop.dragInfo;
-        if (gameObject)
+        if (gameObject && gameObject.transform.parent != transform)
         {
             gameObject.transform.SetParent(transform);
             gameObject.GetComponent<Image>().raycastTarget = true;
             int index = gameObject.GetComponent<CommandInfo>().aiNum - 1;
             commandManager.ExecuteSwitchLine(index);
 
+            // Á¤·Ä
+            var childs = transform.GetComponentsInChildren<CommandInfo>();
+            System.Array.Sort(childs, CompareByAINum);
+
+            foreach (var child in childs)
+            {
+                int childIndex = child.aiNum - 1;
+                child.transform.SetSiblingIndex(childIndex);
+            }
         }
 
-        var childs = transform.GetComponentsInChildren<CommandInfo>();
-        System.Array.Sort(childs, CompareByAINum);
 
-        foreach (var child in childs)
-        {
-            int index = child.aiNum - 1;
-            child.transform.SetSiblingIndex(index);
-        }
     }
 
     private int CompareByAINum(CommandInfo a, CommandInfo b)
