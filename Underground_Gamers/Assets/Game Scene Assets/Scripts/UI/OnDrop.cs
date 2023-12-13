3f98432a8e6572c6a4023d2f8c95bc54d5aa8155
오재영 <ostyoung@naver.com>
2023-12-13 10:32:00 +0900

    라인 변경 패널 문제 픽스 / 다른 초상화 위에 드랍을 하는 경우, 라인 변경이 안되는 문제 수정

--- Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/OnDrop.cs
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/OnDrop.cs
@@ -6,20 +6,24 @@ using System.Collections.Generic;
 public class OnDrop : MonoBehaviour, IDropHandler
 {
     public CommandManager commandManager;
+    public GameObject dropPanel;
+
+    private void Start()
+    {
+        Invoke("LateUpdateSize", 0.1f);
+    }
 
     void IDropHandler.OnDrop(PointerEventData eventData)
     {
-
         GameObject gameObject = DragAndDrop.dragInfo;
-        if (gameObject && gameObject.transform.parent != transform)
+        if (gameObject && gameObject.transform.parent != dropPanel.transform)
         {
-            gameObject.transform.SetParent(transform);
+            gameObject.transform.SetParent(dropPanel.transform);
             gameObject.GetComponent<Image>().raycastTarget = true;
             int index = gameObject.GetComponent<CommandInfo>().aiNum - 1;
             commandManager.ExecuteSwitchLine(index);
-
             // Á¤·Ä
-            var childs = transform.GetComponentsInChildren<CommandInfo>();
+            var childs = dropPanel.transform.GetComponentsInChildren<CommandInfo>();
             System.Array.Sort(childs, CompareByAINum);
 
             foreach (var child in childs)
@@ -27,9 +31,14 @@ public class OnDrop : MonoBehaviour, IDropHandler
                 int childIndex = child.aiNum - 1;
                 child.transform.SetSiblingIndex(childIndex);
             }
+
+            Invoke("LateUpdateSize", 0.1f);
         }
+    }
 
-
+    private void LateUpdateSize()
+    {
+        transform.GetComponent<RectTransform>().sizeDelta = dropPanel.GetComponent<RectTransform>().sizeDelta;
     }
 
     private int CompareByAINum(CommandInfo a, CommandInfo b)
