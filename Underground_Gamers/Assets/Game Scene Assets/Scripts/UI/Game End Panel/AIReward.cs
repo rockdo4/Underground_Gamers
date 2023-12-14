a267c3a18e185bb1026f4c798551e02715d1624e
오재영 <ostyoung@naver.com>
2023-12-15 03:30:03 +0900

    딜 그래프, 보상 패널 작업 중

---
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/Game End Panel/AIReward.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AIReward : MonoBehaviour
+{
+    public GameObject ai;
+    public Image illustration;
+    public Image aiClass;
+    public TextMeshProUGUI lv;
+    public Image grade;
+    public TextMeshProUGUI name;
+
+    public Slider expGauage;
+    public float currentEXP;
+    public float maxEXP;
+}
