3baecd32f1ac709b0d001b15ca927fd3838c751e
오재영 <ostyoung@naver.com>
2023-12-14 22:40:50 +0900

    게임 포기 => 로비 씬 이동 완료

--- Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/Button/GiveUp.cs
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/UI/Button/GiveUp.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GiveUp : MonoBehaviour
 {
     public void OnGiveUp()
     {
         Time.timeScale = 1.0f;
+        SceneManager.LoadScene("Lobby Scene");
     }
 }
