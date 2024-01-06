93ff5314a327dd617269f6d923f60c5002b9e3ca
오재영 <ostyoung@naver.com>
2024-01-06 22:02:28 +0900

    카메라 무빙 화면 비율 비례 완료

--- Underground_Gamers/Assets/Game Scene Assets/Scripts/Manager/CameraMovePanel.cs
+++ Underground_Gamers/Assets/Game Scene Assets/Scripts/Manager/CameraMovePanel.cs
@@ -6,24 +6,29 @@ using UnityEngine.EventSystems;
 public class CameraMovePanel : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     public Camera mainCamera;
-    public float dragSpeed = 2f;
+    public float dragSpeed = 0.8f;
 
     public float minZClamp = -15f;
     public float maxZClamp = 15f;
+
+    private float screenWidth;
 
     private Vector2 originalPanelPosition;
     private Vector2 dragOrigin;
     void Start()
     {
         originalPanelPosition = transform.position;
+        screenWidth = Screen.width;
+        Debug.Log(screenWidth);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("드래그중");
         Vector3 deltaPosition = eventData.position - dragOrigin;
-        Vector3 move = new Vector3(0, 0, -deltaPosition.x * dragSpeed);
-        mainCamera.transform.Translate(move * Time.deltaTime, Space.World);
+        float normalizedDeltaX = deltaPosition.x / screenWidth;
+        Vector3 move = new Vector3(0, 0, -normalizedDeltaX) * dragSpeed;
+        mainCamera.transform.Translate(move * Time.unscaledTime, Space.World);
         Vector3 movePos = mainCamera.transform.position;
         movePos.z = Mathf.Clamp(movePos.z, minZClamp, maxZClamp);
         mainCamera.transform.position = movePos;
