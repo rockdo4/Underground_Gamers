c7d618408cebf1941e4371fd4c6405970a04c275
오재영 <ostyoung@naver.com>
2024-01-08 15:30:45 +0900

    내부 테스트 버전 빌드

--- Underground_Gamers/Assets/Universal/Script/UI/StoryTeller/StoryManager.cs
+++ Underground_Gamers/Assets/Universal/Script/UI/StoryTeller/StoryManager.cs
@@ -1,11 +1,7 @@
-using System.Collections;
+using DG.Tweening;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
-using DG.Tweening;
-using UnityEditorInternal;
-using static UnityEditor.Progress;
 
 public class StoryManager : MonoBehaviour
 {
